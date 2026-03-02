0b708b5940af4abf97316e53c3d9bd3f5e2959dd
Sampath-Dz <chandakasampath12@gmail.com>
2026-03-02 22:31:34 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+USERNAME = "1bav1XU7FHg7DMZ.root"
+PASSWORD = "gmtufRIzi79oT6T8"
+HOST = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"
+PORT = 4000
+DATABASE = "my_db"
+
+SSL_CA = r"C:\tidb_python_project\tidb-serverless-ca.pem"
+
+DATABASE_URL = (
+    f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ca": SSL_CA
+        }
+    }
+)
+
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+print("✅ Connected to TiDB Cloud")
