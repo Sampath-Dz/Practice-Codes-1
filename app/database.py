2a21b397f86e86752ba1db572a93f50c3574f807
Sampath-Dz <chandakasampath12@gmail.com>
2026-03-04 11:12:35 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,19 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 USERNAME = "1bav1XU7FHg7DMZ.root"
-PASSWORD = "LMOo2aLiXZYPGPhZ"
+PASSWORD = "o9ykHR6wPwgNZ39z"
 HOST = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"
 PORT = 4000
 DATABASE = "my_db"
+
 SSL_CA = r"C:\tidb_python_project\tidb-serverless-ca.pem"
 
 DATABASE_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"ssl": {"ca": SSL_CA}},
-    echo=True
+    connect_args={"ssl": {"ca": SSL_CA}}
 )
 
 SessionLocal = sessionmaker(bind=engine)
